64bb822463587d4e478bf70811dce3e85c442d31
nfel <nfilsaraee@gmail.com>
2023-08-14 17:55:28 +0330

    ~

--- tests/checkBal.py
+++ tests/checkBal.py
@@ -9,16 +9,27 @@ rpc = "https://rpc.ankr.com/bsc"
 
 
 def xbalRemote():
-    client = MongoClient("mongodb://localhost:27017/")
+    client = MongoClient("mongodb://piperFinance:Piper2022@localhost:27017/")
     tok = input("tok> ")
     add = input("add> ")
     bl = input("block> ")
-    return (
-        Web3(Web3.HTTPProvider(rpc))
-        .eth.contract(Web3.to_checksum_address(tok), abi=abi)
-        .functions.balanceOf(Web3.to_checksum_address(add))
-        .call(block_identifier=int(bl))
-    ), client.BS_56.UsersBalance.find_one({"tokenStr": tok, "userStr": add})
+    if bl:
+        return (
+            Web3(Web3.HTTPProvider(rpc))
+            .eth.contract(Web3.to_checksum_address(tok), abi=abi)
+            .functions.balanceOf(Web3.to_checksum_address(add))
+            .call(block_identifier=int(bl))
+        ), client.BS_56.UsersBalance.find_one({"tokenStr": tok, "userStr": add})
+    else:
+        return (
+            (
+                Web3(Web3.HTTPProvider(rpc))
+                .eth.contract(Web3.to_checksum_address(tok), abi=abi)
+                .functions.balanceOf(Web3.to_checksum_address(add))
+            ),
+            client.BS_56.UsersBalance.find_one({"tokenStr": tok, "userStr": add}),
+            Web3(Web3.HTTPProvider(rpc)).eth.block_number,
+        )
 
 
 if __name__ == "__main__":
